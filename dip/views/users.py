57e9a15e9158cfa887b0861ab8c932b05737be40
John <birukovie@gmail.com>
2023-04-01 17:23:23 -0400

    add profile_id func and profile_id.html file

--- dip/views/users.py
+++ dip/views/users.py
@@ -55,21 +55,20 @@ def profile():
 
         return redirect(url_for('bp_user.profile'))
 
-@bp.route('/profile/<id_>', methods=['GET', 'POST'])
-@role_required(['user', 'admin'])
-def profile_id(id_):
-    print("OK")
-    # if request.method == 'GET':
-    #
-    #     current_user = get_current_user()
-    #     user_json = current_user.json()
-    #
-    #     if user_json.get('photo'):
-    #         user_json['photo'] = url_for(
-    #             'static.send_user_image', id_=current_user.id)
-    #
-    #     else:
-    #         user_json['photo'] = url_for(
-    #             'static', filename='img/profile-picture.jpg')
-    #
-    #     return render_template('profile.html', user=user_json)
+@bp.route('/profile/<username>', methods=['GET', 'POST'])
+@role_required(['user'])
+def profile_id(username):
+    if request.method == 'GET':
+
+        user = User.query.filter_by(username=username).first()
+        user_json = current_user.json()
+
+        if user_json.get('photo'):
+            user_json['photo'] = url_for(
+                'static.send_user_image', id_=current_user.id)
+
+        else:
+            user_json['photo'] = url_for(
+                'static', filename='img/profile-picture.jpg')
+
+        return render_template('profile_id.html', user=user_json)
